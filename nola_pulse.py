ede164b010d9e647658b32bc184cf378eb6ab919
nola31 <goluprecords@gmail.com>
2025-04-29 09:05:14 +0800

    nola_pulse.py

---
+++ nola_pulse.py
@@ -0,0 +1,27 @@
+nola_pulse.py
+
+import time import threading
+
+class NolaPulse: def init(self, interval_seconds=60): self.interval = interval_seconds self.running = False self.thread = None
+
+def start(self):
+    if not self.running:
+        self.running = True
+        self.thread = threading.Thread(target=self._pulse_loop)
+        self.thread.start()
+
+def stop(self):
+    if self.running:
+        self.running = False
+        if self.thread:
+            self.thread.join()
+
+def _pulse_loop(self):
+    while self.running:
+        print("[Pulse] Прошел еще один интервал.")
+        time.sleep(self.interval)
+
+При запуске файла напрямую
+
+if name == "main": pulse = NolaPulse(interval_seconds=60) pulse.start() try: while True: time.sleep(1) except KeyboardInterrupt: pulse.stop()
+
